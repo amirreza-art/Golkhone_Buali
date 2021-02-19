3cc05aeba968dc93aa5b28a6bf20bf1945c2a263
amirreza <taghaddosiamirreza@gmail.com>
2021-02-19 20:59:47 +0330

    add static methods for ordinary flowers

--- Golkhone/ordinaryflowers.cpp
+++ Golkhone/ordinaryflowers.cpp
@@ -1,6 +1,26 @@
 #include "ordinaryflowers.h"
 
-OrdinaryFlowers::OrdinaryFlowers()
-{
+unsigned int OrdinaryFlowers::ordinaryFlower_count = 0;
 
+OrdinaryFlowers::OrdinaryFlowers(OrdinaryUnion orUnion)
+{
+    this->orUnion = orUnion;
+    ordinaryFlower_count++;
+}
+
+void OrdinaryFlowers::set_ordinaryFlower_count(unsigned int count)
+{
+    ordinaryFlower_count = count;
+}
+void OrdinaryFlowers::inc_ordinaryFlower_count()
+{
+    ordinaryFlower_count++;
+}
+void OrdinaryFlowers::dec_ordinaryFlower_count()
+{
+    ordinaryFlower_count--;
+}
+unsigned int OrdinaryFlowers::get_ordinaryFlower_count()
+{
+    return ordinaryFlower_count;
 }
--- Golkhone/ordinaryflowers.h
+++ Golkhone/ordinaryflowers.h
@@ -2,12 +2,19 @@
 #define ORDINARYFLOWERS_H
 #include <iostream>
 #include "flower.h"
-
+#include "ordinaryunion.h"
 
 class OrdinaryFlowers : public Flower
 {
 public:
-    OrdinaryFlowers();
+    OrdinaryFlowers(OrdinaryUnion);
+    static void set_ordinaryFlower_count(unsigned int);
+    static void inc_ordinaryFlower_count();
+    static void dec_ordinaryFlower_count();
+    static unsigned int get_ordinaryFlower_count();
+private:
+    OrdinaryUnion orUnion;
+    static unsigned int ordinaryFlower_count;
 };
 
 #endif // ORDINARYFLOWERS_H
