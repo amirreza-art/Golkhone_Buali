9c74188592f687b2eec0d978742ae17c49a581c6
amirreza <taghaddosiamirreza@gmail.com>
2021-02-21 17:36:59 +0330

    add destructor and selling price for lilium

--- Golkhone/lilium.cpp
+++ Golkhone/lilium.cpp
@@ -1,10 +1,18 @@
 #include "lilium.h"
 
+extern long int money;
+
 unsigned int Lilium::lilium_count = 0;
 
 Lilium::Lilium()
 {
 
+}
+
+Lilium::~Lilium()
+{
+    money += selling_price;
+    dec_lilium_count();
 }
 
 Lilium::Lilium(OrnamentalFlowerBud ornamentalFlowerBud, LiliumExtract liliumExtract)
@@ -18,14 +26,17 @@ void Lilium::set_lilium_count(unsigned int count)
 {
     lilium_count = count;
 }
+
 void Lilium::inc_lilium_count()
 {
     lilium_count++;
 }
+
 void Lilium::dec_lilium_count()
 {
     lilium_count--;
 }
+
 unsigned int Lilium::get_lilium_count()
 {
     return lilium_count;
--- Golkhone/lilium.h
+++ Golkhone/lilium.h
@@ -7,6 +7,7 @@ class Lilium : public OrnamentalFlowerBud
 {
 public:
     Lilium();
+    ~Lilium();
     Lilium(OrnamentalFlowerBud, LiliumExtract);
     static void set_lilium_count(unsigned int);
     static void inc_lilium_count();
@@ -16,6 +17,7 @@ private:
     OrnamentalFlowerBud ornamentalFlowerBud;
     LiliumExtract liliumExtract;
     static unsigned int lilium_count;
+    static const long int selling_price = 30000;
 };
 
 #endif // LILIUM_H
